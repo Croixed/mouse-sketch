9eb0ecd7def94815c4fe9539ab54df4a2dc8e279
tommy <bt@g>
2022-02-04 18:14:25 -0800

    working slider output to funcs

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const gridContainer = document.querySelector('.grid-container');
 let slider = document.querySelector('.slider');
 
 
-function genPixels(size) {
-  //make a div element with let
-  //apply a class
-  //append it to the grid container
-  console.log(size);
-  return size;
-}
+// function genPixels(size) {
+//   console.log('test');
+//   //make a div element with let
+//   //apply a class
+//   //append it to the grid container
+//   console.log(size + ' inline');
+//   return size;
+// };
 
 
 
@@ -23,4 +24,13 @@ for (let i = 0; i < (14 * 14) - 1; i++) {
   gridContainer.appendChild(pixel);
 };
 
-slider.oninput = genPixels(Number(slider.value));
+function logSize() { //instead of passing in args, like slider.value,
+  console.log(this.value); //use this.value down here. 
+  //or else it just returns a value instead and tries passing that into .oninput but it doesn't accept that
+};
+
+slider.oninput = logSize; // this only accepts funcs not vals
+
+// slider.oninput = function() { // no longer need anony
+//   console.log(this.value);
+// };
