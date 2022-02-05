d70a5895e2010de94fdaeafda784d4d45c0a5430
tommy <bt@g>
2022-02-04 18:32:55 -0800

    working pixel generation

--- script.js
+++ script.js
@@ -2,34 +2,51 @@ const gridContainer = document.querySelector('.grid-container');
 let slider = document.querySelector('.slider');
 
 
-// function genPixels(size) {
-//   console.log('test');
+function genPixels() {
+  console.log(this.value);
+  //remove any previous divs
+  //make a div element with let
+  //apply a class
+  //append it to the grid container
+  //return this.value;
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  };
+  for (let i = 0; i < (this.value * this.value) - 1; i++) {
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    pixel.textContent = '*';
+    gridContainer.appendChild(pixel);
+  };
+
+};
+
+
+
+
+// for (let i = 0; i < (14 * 14) - 1; i++) {
 //   //make a div element with let
 //   //apply a class
 //   //append it to the grid container
-//   console.log(size + ' inline');
-//   return size;
+//   let pixel = document.createElement('div');
+//   pixel.classList.add('pixel');
+//   pixel.textContent = '*';
+//   gridContainer.appendChild(pixel);
 // };
 
 
 
+slider.oninput = genPixels; // this only accepts funcs not vals
 
-for (let i = 0; i < (14 * 14) - 1; i++) {
-  //make a div element with let
-  //apply a class
-  //append it to the grid container
-  let pixel = document.createElement('div');
-  pixel.classList.add('pixel');
-  pixel.textContent = '*';
-  gridContainer.appendChild(pixel);
-};
 
-function logSize() { //instead of passing in args, like slider.value,
-  console.log(this.value); //use this.value down here. 
-  //or else it just returns a value instead and tries passing that into .oninput but it doesn't accept that
-};
 
-slider.oninput = logSize; // this only accepts funcs not vals
+// function logSize() { //instead of passing in args, like slider.value,
+//   console.log(this.value); //use this.value down here. 
+//   //or else it just returns a value instead and tries passing that into .oninput but it doesn't accept that
+// };
+// slider.oninput = logSize();
+
+
 
 // slider.oninput = function() { // no longer need anony
 //   console.log(this.value);
